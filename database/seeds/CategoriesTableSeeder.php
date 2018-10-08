3fbc3d82f9091cbb4ee3f758c0fdaf90250446f2
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2018-10-08 07:00:49 -0600

    Seed Categories Table

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'name' => 'accrued_interest',
+            'standard' => true,
+            'hidden' => true,
+            'notifications' => false
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'General',
+            'standard' => true
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Uncategorized',
+            'standard' => true
+        ]);
+    }
+}

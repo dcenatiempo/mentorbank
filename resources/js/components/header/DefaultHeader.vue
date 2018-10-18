7183eadbd949fc364df33883e2cca8db4daf473c
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2018-10-17 23:36:00 -0600

    Create logout component

--- resources/js/components/header/DefaultHeader.vue
+++ resources/js/components/header/DefaultHeader.vue
@@ -10,71 +10,22 @@
             <ul>
                 <li><a href="/profile">profile</a></li>
                 <li><a href="/bank">bank</a></li>
-                <li><account-icon/></li>
-                <li>{{vpWidth}}</li>
+                <li><router-link to="/bank/profile"><account-icon/></router-link></li>
+                <li><logout></logout></li>
             </ul>
         </nav>
-        <!-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-
-                    <ul class="navbar-nav ml-auto">
-
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> -->
-        
     </header>
 </template>
 
 <script>
 import {mapState, mapGetters, mapActions, mapMutations} from 'vuex';
 import AccountIcon from 'icons/Account';
+import Logout from './Logout';
 
 export default {
     components: {
-        'account-icon': AccountIcon
+        AccountIcon,
+        Logout
     },
     props: {},
     data() {
@@ -83,7 +34,7 @@ export default {
         };
     },
     computed: {
-        ...mapState('app', ['vpWidth']),
+        // ...mapState(),
         // ...mapGetters()
     },
     methods: {
---
+++ resources/js/components/header/Logout.vue
@@ -0,0 +1,27 @@
+<template>
+    <a href="/logout" v-on:click.prevent="logout">
+        Logout
+        <form id="logout-form" action="/logout" method="POST" style="display: none;">
+            <input type="hidden" name="_token" :value="csrf">
+        </form>
+    </a>          
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        }
+    },
+    methods: {
+        logout() {
+            document.getElementById('logout-form').submit();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

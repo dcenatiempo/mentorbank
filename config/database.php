0cba3be3ebf85737be74b106028fb8f3b15358fc
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2018-11-10 17:10:13 -0700

    change database confif for heroku

--- config/database.php
+++ config/database.php
@@ -1,4 +1,11 @@
 <?php
+$url = parse_url(env("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+$port = $url["port"];
 
 return [
 
@@ -56,11 +63,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $host),
+            'port' => env('DB_PORT', $port),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
